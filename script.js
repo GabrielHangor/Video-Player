9587147c5911f8689474c5dd8dc75d7fb9b92df1
HangorG <gaba93@yandex.ru>
2020-11-30 00:14:48 +0300

    play btn functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const video = document.querySelector("video");
 const progressRange = document.querySelector(".progress-range");
 const progressBar = document.querySelector(".progress-bar");
-const playButton = document.querySelector("#play-btn");
+const playButton = document.querySelector("#play-button");
 const volumeIcon = document.querySelector("#volume-icon");
 const volumeRange = document.querySelector(".volume-range");
 const volumeBar = document.querySelector(".volume-bar");
@@ -11,6 +11,19 @@ const fullscreenBtn = document.querySelector(".fullscreen");
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+  playButton.classList.replace("fa-pause", "fa-play");
+  playButton.setAttribute("title", "Play");
+}
+
+function togglePlay() {
+  video.paused
+    ? (video.play(),
+      playButton.classList.replace("fa-play", "fa-pause"),
+      playButton.setAttribute("title", "Pause"))
+    : (video.pause(), showPlayIcon());
+}
+
 // Progress Bar ---------------------------------- //
 
 // Volume Controls --------------------------- //
@@ -18,3 +31,8 @@ const fullscreenBtn = document.querySelector(".fullscreen");
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
+
+// Event listeners
+playButton.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener('ended', showPlayIcon);
