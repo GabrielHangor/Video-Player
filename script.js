fbc975269384021fcbb3c47f49985b23bf022d02
HangorG <gaba93@yandex.ru>
2020-11-30 23:26:26 +0300

    volume bar functionality

--- script.js
+++ script.js
@@ -35,7 +35,6 @@ function displayTime(time) {
   return `${minutes}:${seconds}`;
 }
 
-
 // Update the time/duration in DOM
 function updateProgress() {
   progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
@@ -45,15 +44,34 @@ function updateProgress() {
 
 // Set a specific moment of the video
 function setProgress(e) {
-  const newTime = e.offsetX / progressRange.offsetWidth * 100;
+  const newTime = (e.offsetX / progressRange.offsetWidth) * 100;
   progressBar.style.width = `${newTime}%`;
-  video.currentTime = newTime * video.duration / 100;
-
-  e.preventDefault();
+  video.currentTime = (newTime * video.duration) / 100;
 }
 
 // Volume Controls --------------------------- //
 
+function changeVolume(e) {
+  let volume = (e.offsetX / volumeRange.offsetWidth) * 100;
+
+  if (volume < 10) {
+    volume = 0;
+  } else if (volume > 90) {
+    volume = 100;
+  }
+  volumeBar.style.width = `${volume}%`;
+  video.volume = volume / 100;
+
+  // Change volume icon
+  volumeIcon.className = "";
+  if (volume > 70) {
+    volumeIcon.classList.add("fas", "fa-volume-up");
+  } else if (volume < 70 && volume > 0) {
+    volumeIcon.classList.add("fas", "fa-volume-down");
+  } else if (volume === 0) {
+    volumeIcon.classList.add("fas", "fa-volume-off");
+  }
+}
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
@@ -64,4 +82,5 @@ video.addEventListener("click", togglePlay);
 video.addEventListener("ended", showPlayIcon);
 video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
-progressRange.addEventListener('click', setProgress);
+progressRange.addEventListener("click", setProgress);
+volumeRange.addEventListener("click", changeVolume);
