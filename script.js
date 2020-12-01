2e4a988bed09c373baf7b0a6f42b1373c7f33e17
HangorG <gaba93@yandex.ru>
2020-12-01 23:14:42 +0300

    mute/unmute on click

--- script.js
+++ script.js
@@ -51,6 +51,8 @@ function setProgress(e) {
 
 // Volume Controls --------------------------- //
 
+let lastVolume = 1;
+
 function changeVolume(e) {
   let volume = (e.offsetX / volumeRange.offsetWidth) * 100;
 
@@ -72,6 +74,24 @@ function changeVolume(e) {
     volumeIcon.classList.add("fas", "fa-volume-off");
   }
 }
+
+// Mute / Unmute on click
+function toggleMute() {
+  volumeIcon.className = "";
+  if (video.volume) {
+    lastVolume = video.volume;
+    video.volume = 0;
+    volumeBar.style.width = 0;
+    volumeIcon.classList.add("fas", "fa-volume-mute");
+    volumeIcon.setAttribute("title", "Unmute");
+  } else {
+    video.volume = lastVolume;
+    volumeBar.style.width = `${lastVolume * 100}%`;
+    volumeIcon.classList.add("fas", "fa-volume-up");
+    volumeIcon.setAttribute("title", "Unmute");
+  }
+}
+
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
@@ -84,3 +104,4 @@ video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
+volumeIcon.addEventListener("click", toggleMute);
